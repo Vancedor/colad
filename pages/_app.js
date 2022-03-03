16ae9e2c56fe440ffa2d32948abff0b0c2acc794
Vancedor <46715524+Vancedor@users.noreply.github.com>
2022-03-03 01:15:19 -0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -35,7 +35,7 @@ export default class MyApp extends App {
             <>
                 <Head>
                     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-                    <title>Pungent - React Startup & Digital Agency Multipurpose Template</title>
+                    <title>Collabadda - India's fastest growing influencer marketing Agency</title>
                 </Head>
 
                 <Component {...pageProps} />
