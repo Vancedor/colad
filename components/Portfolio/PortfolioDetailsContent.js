030b768e776ab4d84fac485e8fcc37a692fa9918
Vancedor <46715524+Vancedor@users.noreply.github.com>
2022-03-04 11:24:43 -0800

    Update PortfolioDetailsContent.js

--- components/Portfolio/PortfolioDetailsContent.js
+++ components/Portfolio/PortfolioDetailsContent.js
@@ -111,54 +111,54 @@ class PortfolioDetailsContent extends Component {
                         <div className="row">
                             <div className="col-lg-12">
                                 <div className="portfolio-desc">
-                                    <h2>We Provide Useful Services</h2>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim.</p>
+                                    <h2>Idea Behind Influencer Marketing</h2>
+                                    <p>At a fundamental level, influencer marketing is a type of social media marketing that uses endorsements and product mentions from influencers–individuals who have a dedicated social following and are viewed as experts within their niche. Influencer marketing works because of the high amount of trust that social influencers have built up with their following, and recommendations from them serve as a form of social proof to your brand’s potential customers.</p>
                                     
-                                    <h2>Core Development</h2>
-                                    <p>No fake products and services. The customer is king, their lives and needs are the inspiration. But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids pleasure itself, because it is pleasure, but because those who do not know how to pursue pleasure rationally encounter consequences that are extremely painful.</p>
+                                    <h2>Set a budget and management strategy</h2>
+                                    <p>Now that you have some idea of what to pay influencers, you need to create your budget. Be sure to also factor in time for planning, executing and reviewing your influencer program. Running a successful influencer marketing campaign is not a set-it-and-go type of strategy. It’ll involve careful monitoring and follow up.</p>
 
-                                    <h2>Define Your Choices</h2>
-                                    <p>At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus id quod maxime placeat facere possimus, omnis voluptas assumenda est, omnis dolor repellendus. Temporibus autem quibusdam et aut officiis debitis aut rerum necessitatibus saepe eveniet ut et voluptates repudiandae sint et molestiae non recusandae. Itaque earum rerum hic.</p>
-
-                                    <p>On the other hand, we denounce with righteous indignation and dislike men who are so beguiled and demoralized by the charms of pleasure of the moment, so blinded by desire, that they cannot foresee the pain and trouble that are bound to ensue; and equal blame belongs to those who fail in their duty through weakness of will, which is the same as saying through shrinking from toil and pain. These cases are perfectly simple and easy to distinguish. In a free hour, when our power of choice is untrammelled and when nothing prevents our being able to do what we like best, every pleasure is to be welcomed and every pain avoided. But in certain circumstances and owing to the claims of duty or the obligations of business it will frequently occur that pleasures have to be repudiated and annoyances accepted. The wise man therefore always holds in these matters to this.</p>
+                                    <h2>Decide on goals and message</h2>
+                                    <p>The two most common reasons for using influencer marketing are to elevate brand awareness and increase sales. However, instead of setting these broad targets as your two goals, it will be more effective to kick off on your strategy by honing in on what your brand’s needs are. Perhaps you want to increase your customer base in a younger demographic. Or you want to expand into a new user group with a new product. Or you want to skip trends and utilize influencers to talk about your brand values.</p>
+                                        
+                                    <p>Influencers are here to stay but how the world of influencer marketing looks and operates has changed a great deal in a short time, and in five years may be drastically different from today. This guide will help you get started with building your strategy, but like any social strategy it’s important to be ready for change.</p>
                                 </div>
                     
                                 <div className="service-details-info">
                                     <div className="single-info-box">
                                         <h4>Client</h4>
-                                        <span>James Anderson</span>
+                                        <span>Chingari</span>
                                     </div>
 
                                     <div className="single-info-box">
                                         <h4>Category</h4>
-                                        <span>Saas, Marketing</span>
+                                        <span>Influencer Marketing</span>
                                     </div>
 
                                     <div className="single-info-box">
                                         <h4>Date</h4>
-                                        <span>February 28, 2020</span>
+                                        <span>February 28, 2019</span>
                                     </div>
 
                                     <div className="single-info-box">
                                         <h4>Share</h4>
                                         <ul className="social">
                                             <li>
-                                                <a href="https://www.facebook.com/" target="_blank">
+                                                <a href="https://www.collabadda.in" target="_blank">
                                                     <i className="icofont-facebook"></i>
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="https://twitter.com/" target="_blank">
+                                                <a href="https://twitter.com/collabadda" target="_blank">
                                                     <i className="icofont-twitter"></i>
                                                 </a>
                                             </li>
                                             <li>
-                                                <a href="https://icofont.com/" target="_blank">
+                                                <a href="https://icofont.com/collabadda" target="_blank">
                                                     <i className="icofont-linkedin"></i>
                                                 </a>
                                             </li>
                                             <li> 
-                                                <a href="https://www.instagram.com/" target="_blank">
+                                                <a href="https://www.instagram.com/collabadda" target="_blank">
                                                     <i className="icofont-instagram"></i>
                                                 </a>
                                             </li>
