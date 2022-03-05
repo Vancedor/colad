aa5338c9dc900a2077019f6e0b723b4db026b09b
Vancedor <46715524+Vancedor@users.noreply.github.com>
2022-03-04 21:40:47 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ import About from '../components/MarketingAgencyTwo/About';
 import FunFacts from '../components/Common/FunFacts';
 import PartnerTwo from '../components/Common/PartnerTwo';
 import CtaAreaTwo from '../components/Common/CtaAreaTwo';
-import LatestNews from '../components/Common/LatestNews';
+//import LatestNews from '../components/Common/LatestNews';
 import Footer from '../components/Layouts/Footer';
 
 class MarketingAgency2 extends Component {
